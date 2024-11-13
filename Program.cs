b85197ecdd39c6545006470716a91c5678ce788f
Vulnorn <mixadrbk31@gmail.com>
2024-11-13 13:28:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NewC_3_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineWithText = "You win some";
+
+            string[] words = lineWithText.Split(' ');
+
+            foreach (string word in words)
+            {
+                Console.WriteLine($"{word}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
